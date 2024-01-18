cf2c62947feb4064c242c7d8c30cc8cbb3c4061e
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-19 02:08:17 +1300

    Add constants for done message and cutoff message

---
+++ logic/constants/constants.py
@@ -0,0 +1,2 @@
+DONE_MESSAGE = ' 🏆 Done exporting all progress report cards for students '
+CUTOFF_MESSAGE = '  If you\'ve submitted completed exercises after receiving this email, please inform your trainers. '
