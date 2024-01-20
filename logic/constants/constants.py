acaf8c904331ad63a46363f986c46de322cb705d
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-20 23:14:05 +1300

    Update progress report card messages

--- logic/constants/constants.py
+++ logic/constants/constants.py
@@ -1,3 +1,6 @@
-DONE_MESSAGE = ' 🏆 Done exporting all Progress Report Cards for the Students '
 CUTOFF_MESSAGE = '  If you\'ve submitted completed exercises after receiving this email, please inform your trainers. '
+DONE_MESSAGE = ' 🏆 Done exporting all Progress Report Cards for the Students '
 START_MESSAGE = ' 🟢 Started printing the Report Cards . .. '
+
+START_MESSAGE_PER = ' 🟢 Started printing the Report Card for {}'
+DONE_MESSAGE_PER = ' 🏆 Done export Progress Report Card for {} '
