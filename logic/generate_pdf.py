4bd756dc9ebba69d9a41ea41a40ae41aa0646911
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-21 06:14:34 +1300

    Remove commented out code in generate_pdf.py

--- logic/generate_pdf.py
+++ logic/generate_pdf.py
@@ -153,15 +153,4 @@ def generate_pdf(md_csv_pdf_file, csv_file_path, output_file_name):
     elements.append(Paragraph(dt_string, legend_style))
     elements.append(Paragraph(CUTOFF_MESSAGE, cutoff_style))
 
-    # elements.append(Spacer(1, 12))
-    # elements.append(Spacer(1, 12))
-    # elements.append(Paragraph("Fast Track:", legend_style))
-    # for module, count in exercise_count_per_module.items():
-    #     elements.append(Paragraph(f"{module}: {count} exercises", legend_style))
-
-    # elements.append(Spacer(1, 12))
-    # elements.append(Paragraph("Unsubmitted:", legend_style))
-    # for module, exercises in unsubmitted_exercises.items():
-    #     elements.append(Paragraph(f"{module}: {', '.join(exercises)} are unsubmitted", legend_style))
-
     doc.build(elements)
