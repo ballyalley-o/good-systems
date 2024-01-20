cdb9456de4c2d7f53dfb5f4a067e296b2cef61ec
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-20 23:26:45 +1300

    Update start message and done message in constants.py

--- logic/constants/constants.py
+++ logic/constants/constants.py
@@ -1,6 +1,6 @@
 CUTOFF_MESSAGE = '  If you\'ve submitted completed exercises after receiving this email, please inform your trainers. '
 DONE_MESSAGE = ' 🏆 Done exporting all Progress Report Cards for the Students '
-START_MESSAGE = ' 🟢 Started printing the Report Cards . .. '
+START_MESSAGE = ' 📇 Started printing the Report Cards . .. '
 
-START_MESSAGE_PER = ' 🟢 Started printing the Report Card for {}'
+START_MESSAGE_PER = ' 📇 Started printing the Report Card for {}'
 DONE_MESSAGE_PER = ' 🏆 Done export Progress Report Card for {} '
