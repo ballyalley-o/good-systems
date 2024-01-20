781fc02e755b9efb87673b3e1b2eb3b4abd0a413
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-21 06:14:58 +1300

    Add CSV file conversion and processing logic

--- logic/module_count.py
+++ logic/module_count.py
@@ -1,3 +1,31 @@
+import os
+import csv
+import io
+import pandas as pd
+from dotenv import load_dotenv
+load_dotenv()
+
+md_file_path = os.getenv('PATH_MD')
+csv_file_path = os.getenv('PATH_STUDENT_CSV')
+
+with open(md_file_path, 'r') as file:
+    md_table = file.read()
+
+# Convert MD table to DataFrame
+df = pd.read_csv(io.StringIO(md_table), delimiter=',')  # Change '|' to ','
+
+# Write DataFrame to CSV file
+df.to_csv(csv_file_path, index=False)
+
+# Print the converted CSV file
+print(f"CSV file: {csv_file_path}")
+
+# Read the CSV file into a list of lists
+with open(csv_file_path, 'r') as file:
+    reader = csv.reader(file)
+    list2 = list(reader)
+    # print(f"rows: {list2}")
+
 def count_exercises_per_module(data):
     exercise_count_per_module = {}
     unsubmitted_exercises = {}
@@ -9,18 +37,15 @@ def count_exercises_per_module(data):
         if 'M' not in module_name:
             module_name = 'Pre-work'
 
-        if exercise_status == '✓':
-            if module_name not in exercise_count_per_module:
-                exercise_count_per_module[module_name] = 1
-            else:
-                exercise_count_per_module[module_name] += 1
-        else:
-            if module_name not in exercise_count_per_module:
-                exercise_count_per_module[module_name] = 0
+        if module_name not in exercise_count_per_module:
+            exercise_count_per_module[module_name] = 0
 
-            if module_name not in unsubmitted_exercises:
-                unsubmitted_exercises[module_name] = [f"Exercise {index + 1}" for index, status in enumerate(row[3:], start=1) if status != '✓']
-            else:
-                unsubmitted_exercises[module_name].extend([f"Exercise {index + 1}" for index, status in enumerate(row[3:], start=1) if status != '✓'])
+        if exercise_status != '✓':
+            missing_exercises = [f"Exercise {index + 1}" for index, status in enumerate(row[3:], start=1) if status != '✓']
+            unsubmitted_exercises.setdefault(module_name, []).extend(missing_exercises)
+            exercise_count_per_module[module_name] += 1
+
+    print(f"unsubmitted_exercises before processing: {unsubmitted_exercises}")
 
     return exercise_count_per_module, unsubmitted_exercises
+
