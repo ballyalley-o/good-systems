801a3ce76bcc94ba0213ad6000b2493e1ab45e65
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-21 18:09:42 +1300

    Refactor md_to_csv function and remove unused code

--- md-csv.py
+++ md-csv.py
@@ -1,107 +1,51 @@
 import csv
 import os
-from constants.constants import *
-from colorama import Fore, Style, Back
+from colorama import Fore, Style
 from dotenv import load_dotenv
 load_dotenv()
 
-def md_to_csv(md_table, selected_column_index):
 
+def md_to_csv(md_table):
     md_table = md_table.replace('```', '')
     md_table = md_table.replace('**', '')
 
     lines = md_table.split('\n')
 
-    lines = [lines[0]] + [line for line in lines[1:] if 'Derek' not in line]
+    exclude = os.getenv('EXCLUDE')
 
-    lines = lines[:1] + lines[2:]
+    lines = [lines[0]] + [line for line in lines[1:] if exclude not in line]
+
+    # lines = lines[:1] + lines[2:]
+
+    lines = [line.strip() for line in lines if line]
+
+    header = [value.strip() for value in lines[0].split('|') if value]
+    data = [line.split('|') for line in lines[2:]]
 
     csv_lines = []
-    student_table = []
 
-    for line in lines:
+    for values in data:
+        values = [value.strip() for value in values if value]
+        csv_lines.append(values)
 
-        values = line.split('|')
+    return header, csv_lines
 
-        values = [v.strip() for v in values]
-
-        if len(values) >= (selected_column_index + 1):
-
-            first_column = values[1]
-            second_column = values[2]
-
-            selected_column_value = values[selected_column_index]
-
-            csv_line = f"{first_column},{second_column},{selected_column_value}"
-
-            csv_lines.append(csv_line)
-
-            extracted_values = f"{first_column},{second_column},{selected_column_value}"
-
-            student_table.append(extracted_values)
-
-    csv_table = '\n'.join(csv_lines)
-
-    return csv_table, student_table
-
-def md_to_csv_file(md_file_path, csv_file_path, selected_column_index):
-    """
-    Convert a Markdown table from a file to a CSV file and save it.
-    Additionally, extract a selected column from the Markdown table and save it as a separate CSV file.
-
-    Args:
-        md_file_path (str): The path to the Markdown file.
-        csv_file_path (str): The path to save the converted CSV file.
-        selected_column_index (int): The index of the column to extract and save as a separate CSV file.
-
-    Returns:
-        None
-    """
-
+def md_to_csv_file(md_file_path, csv_file_path):
     with open(md_file_path, 'r') as file:
         md_table = file.read()
 
-    csv_table, student_table = md_to_csv(md_table, selected_column_index)
+    header, csv_lines = md_to_csv(md_table)
 
     with open(csv_file_path, 'w', newline='') as file:
         writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(csv_lines)
 
-        rows = [row.split(',') for row in csv_table.split('\n')]
-        writer.writerows(rows)
+    print('\n')
+    print(Fore.YELLOW + f' 🏆 All Students Progress CSV file saved to {csv_file_path} '  + Style.RESET_ALL)
+    print('\n')
 
-
-    student_table_str = '\n'.join(student_table)
-
-    output_file_name = rows[0][2]
-
-    selected_file_path_template = os.getenv('PATH_CSV_SELECT')
-    selected_file_path = selected_file_path_template.replace('{output_file_name}', output_file_name)
-
-    with open(selected_file_path, 'w', newline='') as file:
-        writer = csv.writer(file)
-
-        rows = [row.split(',') for row in student_table_str.split('\n')]
-        writer.writerows(rows)
-
-    print(Fore.YELLOW + f' 〉MD file loaded:   {md_file_path} ' + Style.RESET_ALL)
-    print(Back.GREEN + Fore.WHITE + f' 〉CSV file saved:  {csv_file_path} ' + Style.RESET_ALL)
-    print(Fore.YELLOW + f' 〉{output_file_name}\'s record saved:  {selected_file_path} ' + Style.RESET_ALL)
-
-# file paths
 md_file_path = os.getenv('PATH_MD')
-csv_file_path = os.getenv('PATH_PROGRESS_CSV')
+csv_file_path = os.getenv('PATH_ALL_CSV')
 
-selected_column_index = 4
-
-for i in range(4, 21):
-    selected_column_index = i
-    current_csv_file_path_template = os.getenv('PATH_CSV_LOOP')
-    current_csv_file_path = current_csv_file_path_template.replace('{i}', str(i))
-    md_to_csv_file(md_file_path, current_csv_file_path, selected_column_index)
-
-print('\n')
-print(Fore.GREEN + DONE_MESSAGE + Style.RESET_ALL)
-print('\n')
-
-
-
+md_to_csv_file(md_file_path, csv_file_path)
