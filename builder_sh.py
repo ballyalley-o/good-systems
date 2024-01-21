bc54048ff8adf1dc03535f722aeeb077da589ae5
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-21 16:18:32 +1300

    Add missing exercise count and print totals (connection to logic/m_count.py)

--- builder_sh.py
+++ builder_sh.py
@@ -1,12 +1,21 @@
+import os
+import sys
+from colorama import Fore, Style, Back
+from logic.m_count import count_missing_exercises
+from dotenv import load_dotenv
+load_dotenv()
 
+student_name = sys.argv[1]
+csv_file_path_template = os.getenv('PATH_STUDENT_CSV')
+csv_file_path = csv_file_path_template.replace('{student_name}', student_name)
 
-elements.append(Spacer(1, 12))
-elements.append(Spacer(1, 12))
-elements.append(Paragraph("Fast Track:", legend_style))
-for module, count in exercise_count_per_module.items():
-    elements.append(Paragraph(f"{module}: {count} exercises", legend_style))
+missing_counts, in_complete, locked, un_accepted = count_missing_exercises(csv_file_path)
 
-elements.append(Spacer(1, 12))
-elements.append(Paragraph("Unsubmitted:", legend_style))
-for module, exercises in unsubmitted_exercises.items():
-    elements.append(Paragraph(f"{module}: {', '.join(exercises)} are unsubmitted", legend_style))
+print("----------------------------------")
+print(Fore.WHITE + Back.LIGHTBLACK_EX + f" TOTALS:                        ⏚ " + Style.RESET_ALL)
+print('\n')
+print(Fore.RED + f" MISSING: {sum(missing_counts.values())} " +  Style.RESET_ALL)
+print(Fore.YELLOW + f" INCOMPLETE: {sum(in_complete.values())} " +  Style.RESET_ALL)
+print(Fore.LIGHTBLACK_EX + f" LOCKED: {sum(locked.values())} " +  Style.RESET_ALL)
+print(Fore.LIGHTWHITE_EX + f" UNACCEPTED: {sum(un_accepted.values())} " + Style.RESET_ALL)
+print('\n')
