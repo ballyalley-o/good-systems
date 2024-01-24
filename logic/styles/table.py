ee79f5c6f645cec8b7e76b3befa04261d67364d9
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-24 23:24:55 +1300

    Add styles_table_main and styles_table_completed functions

---
+++ logic/styles/table.py
@@ -0,0 +1,23 @@
+def styles_table_main(data):
+
+    styles_table = [
+        ('GRID', (0, 0), (-1, -1), 1, (0, 0, 0)),
+        ('BACKGROUND', (0, 0), (-1, 0), 'black'),
+        ('TEXTCOLOR', (0, 0), (-1, 0), 'white'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('FONTSIZE', (0, 0), (-1, 0), 10),
+    ]
+
+    return styles_table
+
+def styles_table_completed(data):
+
+    styles_completed = [
+            ('GRID', (0, 0), (-1, -1), 1, (1, 1, 1)),
+            ('BACKGROUND', (0, 0), (-1, 0), 'green'),
+            ('TEXTCOLOR', (0, 0), (-1, 0), 'white'),
+            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+            ('FONTSIZE', (0, 0), (-1, 0), 10),
+        ]
+
+    return styles_completed
