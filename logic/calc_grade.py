4d18f5945684da9d5797a6e630eddb14525e4849
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-28 04:33:04 +1300

    ✨ feat(Add logic to calculate and print grades  This commit adds two new files, `calc_grade.py` and `calc_grade_print.py`, which contain logic to calculate and print grades based on a CSV file. The `calc_grade.py` file calculates the grades and prints them to the console, while the `calc_grade_print.py` file returns the grades as a dictionary.):

---
+++ logic/calc_grade.py
@@ -0,0 +1,51 @@
+import os
+import csv
+import re
+from colorama import Fore, Back, Style
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def extract_module_number(module_name):
+    match = re.search(r'M(\d+)', module_name)
+    return match.group(1) if match else None
+
+def calculate_grades(csv_file_path):
+    def get_last_name(first_name):
+        return os.getenv(f'{first_name.upper()}')
+
+    with open(csv_file_path, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+
+        total_exercises = {}
+        completed_exercises = {}
+
+        for row in reader:
+            module_name = row[0].rstrip('0123456789')
+            module_no = extract_module_number(module_name)
+            exercise_status = row[2] if len(row) > 2 else None
+
+            if module_no is not None and 'X' not in module_name:
+                total_exercises.setdefault(module_no, 0)
+                total_exercises[module_no] += 1
+
+                if exercise_status == '✓':
+                    completed_exercises.setdefault(module_no, 0)
+                    completed_exercises[module_no] += 1
+
+        grades = {}
+        for module_no in total_exercises.keys():
+            total = total_exercises[module_no]
+            completed = completed_exercises.get(module_no, 0)
+            grades[module_no] = 10 / total * completed
+
+        for module_no, grade in grades.items():
+            print(Fore.YELLOW + f' 〉Module {module_no}: {grade:.2f} ' + Style.RESET_ALL)
+
+        print('\n')
+
+        sum_grade = sum(grades.values())
+        total_grade = sum_grade / len(grades)
+        print(Fore.BLACK + Back.WHITE + f' 〉 Average: {total_grade:.2f} ' + Style.RESET_ALL)
+        print('\n')
