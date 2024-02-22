19309175241503a0cb2b51b865f54ce002755a98
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-23 00:00:26 +1300

    Add styles_table_missing function to table.py

--- logic/styles/table.py
+++ logic/styles/table.py
@@ -26,3 +26,20 @@ def styles_table_completed(data):
         ]
 
     return styles_completed
+
+def styles_table_missing(data):
+
+    styles_missing = [
+                ('BACKGROUND', (0, 0), (-1, 0), colors.ReportLabFidRed),
+                ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+
+                ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                ('FONTSIZE', (0, 0), (-1, 0), 14),
+
+                ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+                ('GRID', (0,0), (-1,-1), 1, colors.black)
+            ]
+
+    return styles_missing
