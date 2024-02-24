cd06624f4e2596ed59f795fd4c2e23fc64fc7d4c
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-24 16:08:48 +1300

    Add import statement for constants module

--- build_rank.py
+++ build_rank.py
@@ -2,6 +2,7 @@ import csv
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
+from logic.constants.constants import *
 
 def get_top_students(csv_file_path):
     with open(csv_file_path, 'r') as f:
@@ -12,11 +13,23 @@ def get_top_students(csv_file_path):
 
         for row in reader:
             # print(f"Row: {row}")  # Debug print
-            if row[1] == 'MINI-PROJECT' and row[0] == '1':
-                for i, grade in enumerate(row[3:], start=3):
+            if row[1] == MINI_PROJECT and row[0] == '1':
+                for i, grade in enumerate(row[2:], start=2):
                     grades[students[i - 2]] = grade
 
+            # if row[1] == 'MINI-PROJECT' and row[0] == '2' and row[2:] == [''] * len(students):
+            #     pass
+            # else:
+            #     for i, grade in enumerate(row[3:], start=3):
+            #         grades[students[i - 2]] = grade
+
+
         print(f"Grades: {grades}")
+
         top_students = sorted(grades, key=grades.get, reverse=True)[:3]
+
+        print(f"Top students: {sorted(grades, key=grades.get, reverse=True)[:3]}")
+        print('\n')
+
         return top_students
 
