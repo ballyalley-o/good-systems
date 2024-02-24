41582f3d81bf270f8689d6c7f42e60cb3dcff7bb
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-24 16:09:09 +1300

    Add MINI-PROJECT constant to constants.py

--- logic/constants/constants.py
+++ logic/constants/constants.py
@@ -8,6 +8,7 @@ START_MESSAGE_PER = ' 📇 Started printing the Report Card for {}'
 DONE_MESSAGE_PER = ' 🏆 Done export Progress Report Card for {} '
 
 REPORT_CARD_TITLE = "Progress Report Card"
+MINI_PROJECT = 'MINI-PROJECT'
 
 # array of values for the legend table
 EXERCISE_STATUS = ['ic', 'L', 'U', 'P']
