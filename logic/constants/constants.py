032d5f3ad456383e225fabd2c423f32fc063ffbb
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-26 21:15:56 +1300

    Add markdown loaded message and update gist file

--- logic/constants/constants.py
+++ logic/constants/constants.py
@@ -6,6 +6,7 @@ START_MESSAGE = ' 📇 Started printing the Report Cards . .. '
 
 START_MESSAGE_PER = ' 📇 Started printing the Report Card for {}'
 DONE_MESSAGE_PER = ' 🏆 Done export Progress Report Card for {} '
+MARKDOWN_LOADED = ' 〉Markdown file loaded: '
 
 REPORT_CARD_TITLE = "Progress Report Card"
 MINI_PROJECT = 'MINI-PROJECT'
@@ -16,6 +17,16 @@ MODULE = 'Module'
 
 MISSING_EXERCISES_MSG = ' ⛔︎ Students MISSING {} - {}:'
 MISSING_EXERCISES_MSG_PDF = " <b>STUDENTS MISSING LAB EXERCISES:</b> "
-CONVERTED_MSG = 'CSV has been CONVERTED to MD'
+CONVERTED_MSG = ' CSV has been CONVERTED to MD '
+GIST_UPDATED_MSG = ' ০ GIST File has been UPDATED. Pushing to gist.github ... '
+
+RESPONSE_SUCCESS = " status: GIST UPDATED, url: {}"
+RESPONSE_FAILED = " FAILED TO UPDATE GIST "
 
 MP1_RANK = 'Mini-Project 1 | ★ TOP {}'
+
+GIST_MD_LINE = 12
+GIST_CUTOFF_ST_MARKER = '<!-- cut here -->'
+GIST_CUTOFF_END_MARKER = '<!-- to here -->'
+
+MARKER_NOT_FOUND = 'Start or end marker not found in the original file.'
