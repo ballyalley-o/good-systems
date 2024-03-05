467d4b6d3e6fa4c60a4a25fa5adcc31dfdb08235
bally <bally.lomibao@howickltd.com>
2024-03-05 23:14:50 +1300

    📃 docs(Refactor send_email function to include optional attachment and add docstring):

--- smpt-students.py
+++ smpt-students.py
@@ -9,26 +9,35 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def send_email(subject, body, to_email, attachment_path, name):
-    # Email configuration
+    """
+    Sends an email with an optional attachment.
+
+    Args:
+        subject (str): The subject of the email.
+        body (str): The body of the email.
+        to_email (str): The recipient's email address.
+        attachment_path (str): The file path of the attachment (optional).
+        name (str): The name of the sender.
+
+    Returns:
+        None
+    """
     sender_email = os.getenv('SMTP_SENDER')
     sender_password = os.getenv('SMTP_PASSWORD')
     smtp_server = os.getenv('SMTP_SERVER')
     smtp_port = os.getenv('SMTP_PORT')
 
-    # Create the email message
     message = MIMEMultipart()
     message["From"] = "Institute of Data"
     message["To"] = to_email
     cc_emails = os.getenv('SMTP_CC_TEST').split(';')
-    # cc_emails = os.getenv('CC_EMAIL')
+
     if cc_emails:
         message["Cc"] = ', '.join(cc_emails)
     message["Subject"] = subject
 
-    # Attach the body of the email
     message.attach(MIMEText(body, "plain"))
 
-    # Attach the file if provided
     if attachment_path:
         with open(attachment_path, "rb") as attachment:
             filename=os.path.basename(attachment_path)
@@ -36,17 +45,13 @@ def send_email(subject, body, to_email, attachment_path, name):
             part['Content-Disposition'] = f'attachment; filename="{filename}"'
             message.attach(part)
 
-    # Establish a secure connection to the SMTP server
     with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
-        # Login to the email account
         server.login(sender_email, sender_password)
-
-        # Send the email
         server.sendmail(sender_email, [to_email] + cc_emails, message.as_string())
 
     print(f"Email with attachment sent successfully to {to_email}")
 
-# Example usage for 16 students with individual attachments
+
 subject = EMAIL_SUBJECT
 
 # TODO: put in .env file
