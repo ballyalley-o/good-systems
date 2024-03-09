909ac95f2f48a44a1b22fe1154732c62cdb51313
bally <bally.lomibao@howickltd.com>
2024-03-09 16:26:29 +1300

    Update email subject for student update

--- smpt-students_per.py
+++ smpt-students_per.py
@@ -7,7 +7,6 @@ from logic.smtp_send_email import send_email
 from dotenv import load_dotenv
 load_dotenv()
 
-subject = EMAIL_SUBJECT
 attachment_abs_path_template = os.getenv('SMTP_ATTACHMENT_ABSOLUTE_PATH')
 
 specific_student_name = sys.argv[1].capitalize()
@@ -24,7 +23,7 @@ with open(os.getenv('SMTP_STUDENT_INFO_TEST'), 'r') as csvfile:
             if len(sys.argv) > 2:
                 if sys.argv[2] == '-u':
                     body = html_template.format(header=os.getenv('SMTP_EMAIL_HEADER'), name=name, emailContent=EMAIL_CONTENT_UPDATE, company=COMPANY_NAME)
-                    send_email(EMAIL_SUBJECT, body, student_email, attachment_path, name)
+                    send_email(EMAIL_SUBJECT_UPDATE, body, student_email, attachment_path, name)
             else:
                 body = html_template.format(header=os.getenv('SMTP_EMAIL_HEADER'), name=name, emailContent=EMAIL_CONTENT, company=COMPANY_NAME)
                 send_email(EMAIL_SUBJECT, body, student_email, attachment_path, name)
