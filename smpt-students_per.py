2d48a46a123d42a45cfc127badf581fd85541e2a
bally <bally.lomibao@howickltd.com>
2024-03-09 15:40:55 +1300

    Add SMTP email functionality to send personalized emails to specific students

---
+++ smpt-students_per.py
@@ -0,0 +1,35 @@
+import os
+import csv
+import sys
+from logic.constants.constants import *
+from logic.constants.html import html_template
+from logic.smtp_send_email import send_email
+from dotenv import load_dotenv
+load_dotenv()
+
+subject = EMAIL_SUBJECT
+attachment_abs_path_template = os.getenv('SMTP_ATTACHMENT_ABSOLUTE_PATH')
+
+# with open(os.getenv('SMTP_STUDENT_INFO'), 'r') as csvfile:
+#     reader = csv.DictReader(csvfile)
+#     for row in reader:
+#         name = row['Firstname']
+#         student_email = row['Email']
+#         attachment_abs_path = attachment_abs_path_template.replace('{student_name}', name)
+#         attachment_path = attachment_abs_path
+#         body = html_template.format(header=os.getenv('SMTP_EMAIL_HEADER'), name=name, emailContent=EMAIL_CONTENT, company=COMPANY_NAME)
+#         send_email(subject, body, student_email, attachment_path, name)
+
+
+specific_student_name = sys.argv[1].capitalize()
+
+with open(os.getenv('SMTP_STUDENT_INFO_TEST'), 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        name = row['Firstname']
+        if specific_student_name == name:
+            student_email = row['Email']
+            attachment_abs_path = attachment_abs_path_template.replace('{student_name}', name)
+            attachment_path = attachment_abs_path
+            body = html_template.format(header=os.getenv('SMTP_EMAIL_HEADER'), name=name, emailContent=EMAIL_CONTENT, company=COMPANY_NAME)
+            send_email(subject, body, student_email, attachment_path, name)
