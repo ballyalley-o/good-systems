89e3b3bb760c91d38aa4e7054531a60f88bedbc5
bally <bally.lomibao@howickltd.com>
2024-03-14 22:15:30 +1300

    Add upcoming mini-projects to check

--- logic/constants/constants.py
+++ logic/constants/constants.py
@@ -13,7 +13,9 @@ MINI_PROJECT = 'MINI-PROJECT'
 
 # array of values for the legend table
 EXERCISE_STATUS = ['ic', 'L', 'U', 'P']
-UPCOMING = ['2', '3']
+# TODO: AUTOMATE THIS - ADDING OF MINI-PROJECTS TO CHECK
+UPCOMING = ['2', 'CAPSTONE']
+DONE = ['1', '3']
 MODULE = 'Module'
 
 MISSING_EXERCISES_MSG = ' ⛔︎ Students MISSING {} - {}:'
@@ -24,7 +26,7 @@ GIST_UPDATED_MSG = ' ০ GIST File has been UPDATED. Pushing to gist.github ... 
 RESPONSE_SUCCESS = " status: GIST UPDATED, url: {}"
 RESPONSE_FAILED = " FAILED TO UPDATE GIST "
 
-MP1_RANK = 'Mini-Project 1 | ★ TOP {}'
+MP_RANK = 'Mini-Project {} | ★ TOP {}'
 
 GIST_MD_LINE = 12
 GIST_CUTOFF_ST_MARKER = '<!-- cut here -->'
